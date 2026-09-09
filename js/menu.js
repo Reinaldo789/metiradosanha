38a288824c0ca5940f551a755db73592f9711079
Reinaldo789 <90279951+Reinaldo789@users.noreply.github.com>
2026-09-09 10:18:59 -0400

    Fix formatting of fecharMenuMobile call

--- js/menu.js
+++ js/menu.js
@@ -361,8 +361,11 @@ document.addEventListener("keydown", function(event) {
 
     if (event.key === "Escape") {
 
-        fecharMenuMobile();
+        fecharMenuMobile() 
+       }
 
-    }
+    });
 
 });
+
+    
